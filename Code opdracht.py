b1c6b31946daaf18e3263f7f164cd58f6764540e
Armelluh <armelle.vonk@hotmail.com>
2026-02-12 21:45:27 +0100

    Add files via upload

---
+++ Code opdracht.py
@@ -0,0 +1,339 @@
+
+############################################################
+# Python assignment for course KT3401                     #
+# 'Intensive Care and Computer Simulation'                #
+# Week 1: Reconstruct patient status from                 #
+# a database with clinical variables                      #
+#                                                         #
+# Author: Thijs Perenboom - Dec 2016                      #
+# 1st revision: Nico Jansen - Dec 2018                    #
+# 2nd revision: Nico Jansen - Nov 2019                    #
+# 3rd revision: Michel Hu/Susana Jorge - October 2020     #
+# 4th revision: Floor Hiemstra, Fleur Brouwer & Siri vd   #
+# Meijden - Sept 2022                                     #
+# 5th revision: Floor Hiemstra & Floor Smits - Sept 2024  #
+############################################################
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sumorders import sumPendingOrders
+
+############################################################
+## Part 1: Patients at the ICU
+############################################################
+# Seven patients are included, as example we're looking at patient #1 with
+# PatientID = 107032. This ID is used as 'key' across databases, and can be
+# used to select data for this specific patient.
+
+# load table TPatients (be sure to add the data folder to your Python path)
+TPatients = pd.read_csv('TPatients.csv')
+
+# Hint: use parse_dates to load dates as datetime objects directly. You can look in the 
+# pandas documentation how to do this. Look for the correct columns to parse as dates.
+
+# Show the PatientID's in the table TPatients. Pandas DataFrames can be indexed
+# by using '''' as separator:
+allPatients = TPatients['PatientID'].values
+print(allPatients)
+
+## Determine the time patient 1 stayed at the ICU
+# Hint: extract the useful data from the table by using PatientID as an
+# indexing tool.
+# Indexing example: you want to index in a simple list of 
+# Years = [1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+# Years[2]  # This will extract only the 3rd element of list Years
+# Output: 1994
+
+admDate = TPatients.loc[TPatients['PatientID'] == allPatients[0], "AdmissionDate"].values[0]
+endDate = []  # add your calculation
+lengthStay = []  # add your calculation
+
+# Hint: you may need to convert the date strings to datetime objects using 
+# pd.to_datetime(date_string)
+
+# You'll find this patient stayed at the ICU for almost 76 hrs
+lengthStay_hours = lengthStay.total_seconds() / 3600
+print(f'This patient stayed at the ICU for {lengthStay_hours} hours')
+
+############################################################
+## Part 2: Measured parameters and their units: weight of patient
+############################################################
+# In table TSignals, all measurements on the patients are combined. The
+# measured parameter is coded for by ParameterID, that is coupled to the
+# table TParameters (here you can find ParameterName and UnitID). Each
+# measurement has a Time of measurement and corresponding Value. UnitID is
+# coupled to table TUnits, where UnitName (e.g. mg, degree Celsius etc) and
+# conversion parameters are stored.
+#
+# First, let's find the weight of patient 1 by loading TSignals, TUnits and
+# TParameters
+TSignals = []
+TUnits = []
+TParameters = []
+
+# We will extract the weight from TSignals for patient 1, by looking for
+# ParameterID == 80. You can verify that this ID is correct by finding the
+# ParameterName corresponding to ParameterID 80.
+
+# This should plot 'Gewicht' (i.e. Weight)
+print(TParameters.loc[TParameters['ParameterID'] == 80, 'ParameterName'])
+
+# Unit of weight: first get the UnitID from TParameters using ParameterID, then 
+# check corresponding UnitName in TUnits.
+weightUnitID = []
+weightUnit = []
+
+# Weight of the first patient, use PatientID and (&) ParameterID
+patWeight = TSignals.loc[[], 'Value']  # Fill in condition
+print(patWeight)
+
+# You should see a variable with size 7x1 with value 95000, indicating that the weight
+# was measured multiple times during the ICU stay. Also extract the times
+# of measurement from table TSignals to check if this is correct.
+weightTimes = []
+
+# You probably noticed that the weight unit (kg) and the Value in TSignals do
+# not correspond because that would mean that the patient has a weight of 95000kg!. 
+# Therefore, you have to convert the Value by subtracting 
+# "TUnits.Addition" and subsequently dividing by "TUnits.Multiplier". This
+# conversion should be done for every parameter you will later extract from
+# the database!
+
+weightAddition = []
+weightMultiplier = []
+
+# Apply the addition and then multiplication correction to scale to
+# UnitName, resulting in the correct patient's weight (95 kg)
+patWeight = patWeight + []
+print(f"\nThe weight of this patient is: {patWeight.iloc[0]:.1f} kg")
+
+############################################################
+## Part 3: Changing measurements over time: cardiac output
+############################################################
+# You are now able to find a specific measurement from a patient and convert
+# it. Let's plot changes in the signal over time, by looking at the Cardiac
+# output of patient 107032. Cardiac output has parameterID = 27 and is also
+# stored in TSignals.
+
+# Get the values and timepoints of cardiac output from patient 107032
+values = []
+time = []
+
+# For plotting purposes, it is easier to subtract the admission time from
+# the timepoints. In that way, your plot starts at 0 and you don't have to
+# bother with dates.
+# It is possible that you need to convert the time difference to hours. 
+# For example bij adding: .astype("timedelta64[s]").astype(float) / 3600. 
+# This depends on how you loaded the dates in earlier parts. 
+time = time - admDate
+
+# Get the unit of cardiac output from TUnits, to use as y-label
+signal_unitID = []
+signal_unit = []
+# And the name of the parameter from TParameters, to use as plot title
+signal_name = []
+
+# Convert the values using the addition and multipliers
+signal_addition = []
+signal_multiplier = []
+values = values + []
+
+# Now let's plot! You can mark each measurement, or draw lines, or a
+# combination. Also experiment with line width and line types.
+plt.figure()
+plt.plot([], [])
+plt.xlabel('What is the correct label for the x-axis?')
+plt.ylabel(signal_unit)
+plt.title(signal_name)
+
+# If you plot more than one parameter (e.g. using subplots), it is useful
+# to have the same x-axis. What would be a good range to plot of these
+# values, that's comparable over all parameters? 
+plt.xlim([])
+
+############################################################
+## Part 4: plotting of multiple parameters
+############################################################
+# Other parameters then Cardiac output are also of interest to check the
+# hemodynamic status of patients. Use a for-loop over multiple
+# parameterID's to (sub)plot all these values by combining the steps you've
+# seen before.
+
+# ParameterID's of interest are: 10, 13, 27, 326, 627, 7500, 7518
+# Note that not all parameters are measured in all patients, so think of a
+# way to use a loop even when a parameter is not measured (hint: you can
+# check if the values vector are 'empty' and in that case 'continue' 
+# the loop).
+
+# Example of a for loop
+# You can use a for loop to repeat a certain action
+# Here we have a simple for loop where we want to add +1 to each element in
+# a list that is 10 elements long
+# (P.S. if you want to continue coding in Python, these types of for loops
+# are usually not recommended because you can use vectorized operations
+# with libraries like NumPy, which are much faster. This example is only
+# meant to illustrate what a for loop is.)
+
+# test = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# new = []
+# for i in range(10):
+#     new.append(test[i] + 1)
+
+plt.figure(figsize=(10, 14))
+plt.suptitle(f'Patient: {allPatients[0]}')
+
+axes = []
+
+for iprm in range([]):
+    # Get all relevant information about the parameter
+    # ...
+
+    # Create subplot and store axis handle
+    ax = plt.subplot([], 1, iprm + 1)
+    axes.append(ax)
+
+    # Do all your plotting per parameter here
+
+
+# Link all subplots along the x-axis
+for ax in axes[1:]:
+    ax.sharex(axes[0])
+
+plt.show()
+
+
+############################################################
+## Part 5: Plotting of Pending Orders: dobutamine
+############################################################
+# Not only can we plot signals that are measured, also pending orders are of
+# interest to monitor the hemodynamic status of a patient. Examples are the
+# administration of medication like dobutamine, or blood transfusions.
+# These values are stored in tables TPendingOrders, TPendingRangeSignals,
+# TPlannedOrders. The last table we need is TDripUnits, containing
+# information about the time parameter of the order (e.g. per minute, per
+# hour etc).
+
+# Load the relevant tables
+TPendingOrders = []
+TPendingRangeSignals = []
+TPlannedOrders = []
+TDripUnits = []
+
+# Dobutamine has parameterID 363, define as variable for later use
+prmID = 363
+
+# TPendingRangeSignals contains StartTime, EndTime and Values of the
+# orders, and the corresponding DripUnitID to look up in TDripUnits.
+# However, we only have to look at orders with Status == 2 in
+# TPendingOrders, only these are really performed (and not just ordered).
+
+# Here, we will combine both tables, by joining them using PendingOrderID
+TPending = pd.merge(TPendingOrders, TPendingRangeSignals, on='PendingOrderID')
+
+# From the created table TPending, lookup the StartTime, EndTime and
+# Values for each Order, based on PatientID, ParameterID (dobutamine in this case) and Status. First,
+# you can create an indices variable as you need the same indices (of all the
+# times that the same order is given) multiple times.
+idcs = []
+startTime = TPending.loc[idcs, 'StartTime'].values - admDate
+endTime = []
+values = []
+
+# Also find the parameter's name in TParameters
+signal_name = []
+
+# And lookup the UnitName and conversion factors
+signal_unitID = []
+signal_unit = []
+sig_multiplier = []
+sig_addition = []
+
+# These values also have a time depending component, that can be found in
+# TDripUnits. But not all parameters have a dripUnit, so you have to
+# account for this by checking if the value is not 'isempty'. First find
+# the corresponding timeUnitID in TDripUnits with the DripUnitID, this timeUnitID
+# can be checked again in TParameters as UnitID to find the unit name.
+
+if ...
+    signal_timeUnitID = []
+    signal_unit_time = []
+    signal_dripUnit_multiplier = []
+    signal_dripUnit_addition = []
+
+# Apply the value conversion, by first converting for the Unit and then for
+# DripUnit. Note that DripUnit is defined 'per time', and should therefore
+# not divided by the multiplier, but multiplied by this number.
+values = values + []
+values = values + []
+
+# Some of these parameters are administered on a continuous time scale, 
+# while others are provided just once per order. This is registered in the
+# table TPlannedOrders by the variable "Continuous". First lookup the
+# PlannedOrderID's for all relevant orders in table TPending (hint: use
+# your earlier defined "idcs" again).
+signal_plannedOrder = []
+
+# Here we check if a parameter is continuously is administered or not
+if len(signal_plannedOrder) > 0:
+    signal_continuous = TPlannedOrders.loc[TPlannedOrders['PlannedOrderID'] == signal_plannedOrder[0], 'Continuous'].values[0]
+else:
+    signal_continuous = 0   # You can leave this 0 as is, it should be there.
+
+# By now, you have all information needed to calculate the administration of
+# dobutamine. This is done by looking at the Value and Time points over
+# the entire stay at the ICU.
+
+# Calculate the total medication dosage over time using the function 
+# sumPendingOrders that was already created for you in the module sumorders. 
+# Check its code to see how to call the function, what it returns and how it works.
+# Try changing the 'resolution' parameter to see what the effect is.
+[timeVec, summedOrder] = sumPendingOrders(lengthStay, signal_plannedOrder, startTime, endTime, values, signal_continuous, 0.25)
+
+# Now you can plot the output of sumPendingOrders as you did with the
+# values from TSignals. Again, try different settings to find out what is
+# the best option for plotting this data.
+plt.figure()
+plt.plot([], [])
+
+
+# The y-label is a bit more complicated, because you also need to include
+# the DripUnit. You can build the label using an f-string inside ax.set_ylabel().
+# Make sure that signal_unit and signal_unit_time are strings; if they come
+# from a pandas DataFrame, they usually already are. Otherwise, convert them
+# using str().
+plt.ylabel([])
+plt.title([])
+plt.xlim([])
+
+############################################################
+## Part 6: Overview of pending orders
+############################################################
+# Other administrations then dobutamine output are also of interest to check the
+# hemodynamic status of patients. Use a for-loop over multiple
+# parameterID's to (sub)plot all these values by combining the steps you've
+# seen before.
+
+# ParameterID's of interest are: 363, 8218, 8202, 18054
+# Note that not all parameters are measured in all patients, so think of a
+# way to use a loop even when a parameter is not measured (hint: you can
+# check if the returned value vector 'isempty').
+
+############################################################
+## Part 7: Other parameters of interest for assessment of hemodynamic status
+############################################################
+# So far, we have plotted parameters using the ParameterID. However, other
+# parameters may be of relevance of assessment of hemodynamic status in
+# this patient. For example, one potential parameter would be heart rate.
+
+# In order to find the relevant ParameterID for heart rate, you can use the 
+# function 'contains()' (hint: note that you can search for parts of a string).
+prmIDi = TParameters['ParameterName'].str.contains([], case=False, na=False)
+print(TParameters[prmIDi])
+
+# Now that you have found the relevant ParameterIDs, plot its values as you
+# did for cardiac output in part 4 (or part 3 if you have a single parameter to plot).
+
+# What other parameters may be informative for evaluating the hemodynamic 
+# status of patients? Use the steps above to recover the ParameterIDs of
+# these parameters and plot them.
---
+++ sumorders.py
@@ -0,0 +1,71 @@
+""" 
+Module with a function to sum pending orders over time. This function can be used in the main code for the assignment.
+"""
+import numpy as np 
+
+def sumPendingOrders(lengthStay, order, startTime, endTime, values, continuous, resolution=0.5):
+    # Calculates the total order administration per parameter, based on start and 
+    # end times as provided in TPendingOrders.
+    # Inputs:
+    # - lengthStay: total duration of the patient's stay at ICU
+    # - order: PlannedOrderID for all orders, to see which orders are part of
+    # the same PlannedOrders (e.g. only change in settings).
+    # - startTime: start of the order for this parameter
+    # - endTime: end of the order for this parameter
+    # - values: corresponding value per start/end time combination
+    # - continuous: is the order applied continuously or not?
+    # - resolution (optional): time resolution in hours [default = half hour (0.5)]
+    #
+    # Outputs:
+    # - t: time vector containing the datetime points per row
+    # - summedOrder: total medication dosage for the order over entire stay
+
+    # Default precision is half hour
+    # (Already implemented with resolution default)
+    
+    # number of different orders
+    nOrder = np.unique(order)
+
+    # create time vector based on precision and lengthStay
+    hours = lengthStay / np.timedelta64(1, 'h')
+    t = np.arange(0, hours + resolution/24, resolution)  
+
+    # Convert start/end times from timedelta64 to hours
+    start_h = startTime / np.timedelta64(1, 'h')
+    end_h = endTime / np.timedelta64(1, 'h')
+
+    # If variable is continuous, an order with equal PlannedOrder is just change of settings, not a new order.
+    if continuous:
+    
+        # create empty matrix to put in values per order over time
+        summedOrder = np.zeros((len(t), len(nOrder)))
+
+        # put values in summedOrder between start and end time
+        for iorder_idx, iorder in enumerate(nOrder):
+            idx = (order == iorder)
+
+            sTime = np.append(start_h[idx], hours)
+            val = values[idx]
+
+            for iidx in range(len(sTime) - 1):
+                mask = (t >= sTime[iidx]) & (t <= sTime[iidx+1])
+                summedOrder[mask, iorder_idx] = val[iidx]
+
+    # If variable is not continuous, base the total administration on start and end times
+    else:
+        # find orders that are positive in time (end time after start time)
+        idx = (end_h - start_h) > 0
+        
+        # create empty matrix to put in values per order over time
+        summedOrder = np.zeros((len(t), np.sum(idx)))
+        
+        # put values in summedOrder between start and end time
+        valid_indices = np.where(idx)[0]
+        for k, i in enumerate(valid_indices):
+            mask = (t >= start_h[i]) & (t <= end_h[i])
+            summedOrder[mask, k] = values[i]
+
+    # sum these values over time, to get total order administration
+    summedOrder = np.sum(summedOrder, axis=1)
+
+    return t, summedOrder
